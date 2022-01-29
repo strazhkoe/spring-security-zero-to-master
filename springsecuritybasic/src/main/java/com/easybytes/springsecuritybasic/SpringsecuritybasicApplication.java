c71f5b26176e32e1f7cd8b48d09e156ac7dc659e
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2022-01-29 22:13:12 +0200

    Added controller, security, default user and password

--- springsecuritybasic/src/main/java/com/easybytes/springsecuritybasic/SpringsecuritybasicApplication.java
+++ springsecuritybasic/src/main/java/com/easybytes/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,8 +2,10 @@ package com.easybytes.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.easybytes.springsecuritybasic.controller")
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
